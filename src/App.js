83b2ac44b3fd778680846efb8c527d95bb9645a7
Melchor De La Rosa <melchordelarosa41@yahoo.com>
2024-11-18 15:48:24 -0500

    Added categories component and styles

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import './categories.styles.scss'
+import Categories from "./components/categories/Categories";
+
 const App = () => {
 
   const categories = [
@@ -31,17 +32,7 @@ const App = () => {
 
   return (
     <div className='categories-container'>
-      { categories.map(({ id, title, imageUrl }) => (
-        <div key={id} className='category-container'>
-        <div className="background-image" style={{
-          backgroundImage: `url(${imageUrl})`
-        }}/>
-        <div className='category-body-container'>
-          <h2>{ title }</h2>
-          <p>Shop Now</p>
-        </div>
-      </div>
-      ))}
+      <Categories categories={ categories }/>
     </div>
   );
 };
---
+++ src/components/categories/Categories.jsx
@@ -0,0 +1,15 @@
+import './categories.scss'
+import CategoryItem from '../category-item/category-item.component'
+
+const Categories = ({ categories }) => {
+	console.log(categories)
+  return (
+    <div className='categories-container'>
+			{ categories.map((category) => (
+        <CategoryItem key={category.id} category={ category }/>
+      ))}
+    </div>
+  )
+}
+
+export default Categories
